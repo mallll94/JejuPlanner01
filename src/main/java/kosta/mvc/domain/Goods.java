d078be7f813c9bdaeb2b26948188e4233f33ffd9
chaejeong <pagw1604@gmail.com>
2022-05-27 20:51:11 +0900

    goods주석 추가

--- src/main/java/kosta/mvc/domain/Goods.java
+++ src/main/java/kosta/mvc/domain/Goods.java
@@ -38,8 +38,9 @@ public class Goods {
 	@JoinColumn(name = "place_fk")
 	private Place place; //장소데이터 번호
 	
+	//마이리얼트립 참고>> 애월, 제주시, 함덕/구좌, 성산/우도, 표선, 서귀포, 중문, 한림/협재
 	private String goodsLocalCategory; //지역카테고리
-	private String goodsCategory;
+	private String goodsCategory; //액티비티, 입장권, 스파/힐링, 대여 
 	private String goodsName;
 	private int goodsPrice;
 	
