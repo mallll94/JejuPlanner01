343c00685ca112bc0f5e399e08442c5b4cdebc0f
이아리 <71698417+LEEAHRI@users.noreply.github.com>
2022-05-28 12:28:52 +0900

    CartService 메소드 작성.

--- src/main/java/kosta/mvc/service/CartService.java
+++ src/main/java/kosta/mvc/service/CartService.java
@@ -1,5 +1,7 @@
 package kosta.mvc.service;
 
+import java.util.List;
+
 import kosta.mvc.domain.Cart;
 
 public interface CartService {
@@ -7,20 +9,24 @@ public interface CartService {
 	/**
 	 * 장바구니 등록
 	 */
-	public void addCart(Cart add);
 	
-
-	/**
-	 * 장바구니 부분 삭제하기
-	 */
+	public void addCart(Cart cart);
 	
 	/**
-	 * 장바구니 전체 삭제하기
+	 * 장바구니 삭제하기
 	 */
+	
+	public void deleteCart(Long cartId);
+	
+	/*
+	 * 장바구니 개별 조회하기
+	 */
+	
+	public Cart getCart(Long cartID);
 	
 	/**
-	 * 장바구니 조회하기
+	 * 유저아이디로 카트 조회하기
 	 */
 	
-
+	public List<Cart> getAllCart(Long userId);
 }
