45814019d74c330aeb95999cfaeca75ff696272a
jeonghyeon <wjdgsu53@naver.com>
2022-05-29 19:20:20 +0900

    Revert "CartService 메소드 작성."
    This reverts commit 343c00685ca112bc0f5e399e08442c5b4cdebc0f.

--- src/main/java/kosta/mvc/service/CartService.java
+++ src/main/java/kosta/mvc/service/CartService.java
@@ -1,7 +1,5 @@
 package kosta.mvc.service;
 
-import java.util.List;
-
 import kosta.mvc.domain.Cart;
 
 public interface CartService {
@@ -9,24 +7,20 @@ public interface CartService {
 	/**
 	 * 장바구니 등록
 	 */
+	public void addCart(Cart add);
 	
-	public void addCart(Cart cart);
+
+	/**
+	 * 장바구니 부분 삭제하기
+	 */
 	
 	/**
-	 * 장바구니 삭제하기
+	 * 장바구니 전체 삭제하기
 	 */
-	
-	public void deleteCart(Long cartId);
-	
-	/*
-	 * 장바구니 개별 조회하기
-	 */
-	
-	public Cart getCart(Long cartID);
 	
 	/**
-	 * 유저아이디로 카트 조회하기
+	 * 장바구니 조회하기
 	 */
 	
-	public List<Cart> getAllCart(Long userId);
+
 }
