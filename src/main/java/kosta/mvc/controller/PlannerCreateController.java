a8deb1887ca9944e83d5df658fe3f227f3166543
chaejeong <pagw1604@gmail.com>
2022-06-01 21:54:22 +0900

    0601날짜 유효성 체크, 프론트단 기능추가

--- src/main/java/kosta/mvc/controller/PlannerCreateController.java
+++ src/main/java/kosta/mvc/controller/PlannerCreateController.java
@@ -3,6 +3,7 @@ package kosta.mvc.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,12 +25,14 @@ public class PlannerCreateController {
 	@Autowired
 	private PlaceService placeService;
 	
+	private final static int PLACE_PAGE_COUNT=10;
+	
 	
 	@RequestMapping("/{url}")
 	public void init() {
 		
 	}
-	
+	/**플래너 전체조회하기*/
 	@RequestMapping("/plannerIndex")
 	public void selectAllByUserID(Model model/* , String userId */){
 		//임시 테스트 아이디
@@ -39,6 +42,7 @@ public class PlannerCreateController {
 		
 	}
 	
+	/**플래너 작성하기1로 이동*/
 	@RequestMapping("/plannerWrite/{plannerId}")
 	public String wirteForm(@PathVariable Long plannerId, Model model) {
 		Planner planner =plannerService.selectBy(plannerId);
@@ -46,24 +50,30 @@ public class PlannerCreateController {
 		return "/planner/plannerWrite";
 	}
 	
-	
-	
+	/**플래너 등록하기 > 플래너 작성하기2로 이동*/
+	@RequestMapping("/plannerInsert")
+	public String insert(Planner planner) {
+		
+		return null;
+	}
+
 	/**오른쪽 사이드바에서 카테고리별 추천받기*/
 	@RequestMapping("/recommend")
 	@ResponseBody
 	public List<Place> recommendCategory(String category) {
-		//개수제한 하는 로직 필요함!!! 전체가져오면 X
-		System.out.println(category);
-		//List<Place> plist =null;
-		//List<Place> plist =placeService.selectByCata("placeSave", category);
-		return null;
+		Page<Place> dblist =placeService.selectByCata("placeSave", category,1,PLACE_PAGE_COUNT);
+		List<Place> plist = dblist.getContent();
+		return plist;
 	}
 	
-	/**오른쪽 사이드바에서 장소추가하기*/
+	
+	
+	/**오른쪽 사이드바에서 장소추가하기 >> 장소 아이디로 장소 검색하기*/
 	@RequestMapping("/addPlace")
 	@ResponseBody
-	public void addPlace() {
-		
+	public Place addPlace(Long placeId) {
+		Place dbplace =placeService.selectById(placeId);
+		return dbplace;
 	}
 	
 	/**왼쪽 사이드바에서 기간정하기*/
