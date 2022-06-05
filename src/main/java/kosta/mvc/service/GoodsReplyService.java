73ef52f4d9ff9116aeb015395a85ef3b577ed3f4
이아리 <71698417+LEEAHRI@users.noreply.github.com>
2022-06-05 22:38:21 +0900

    GoodsReplyService 완성.

--- src/main/java/kosta/mvc/service/GoodsReplyService.java
+++ src/main/java/kosta/mvc/service/GoodsReplyService.java
@@ -2,20 +2,27 @@ package kosta.mvc.service;
 
 import kosta.mvc.domain.GoodsReply;
 
+import java.util.List;
+
 public interface GoodsReplyService {
-	
+
 	/**
 	 * 후기 등록하기
 	 */
 	public void addGoodsReply(GoodsReply goodsReply);
-	
+
 	/**
 	 * 후기 삭제하기
 	 */
 	public void deleteGoodsReply(Long goodsReplyId);
-	
+
 	/**
 	 * 후기 수정하기
 	 */
 	public void updateGoodsReply(GoodsReply goodsReply);
+
+	/**
+	 * 후기 전체 조회하기
+	 */
+	public List<GoodsReply> getAllGoodsReply();
 }
