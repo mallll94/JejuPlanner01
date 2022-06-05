83425ca40ea6772a9cf64f8e26fff5a676ee9565
chaejeong <pagw1604@gmail.com>
2022-06-05 22:42:49 +0900

    추가커밋

--- src/main/java/kosta/mvc/controller/DiaryController.java
+++ src/main/java/kosta/mvc/controller/DiaryController.java
@@ -1,15 +1,21 @@
 package kosta.mvc.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import kosta.mvc.domain.Diary;
+import kosta.mvc.domain.DiaryLine;
 import kosta.mvc.domain.Users;
+import kosta.mvc.dto.DiaryLineDTO;
 import kosta.mvc.service.DiaryService;
 import kosta.mvc.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -40,8 +46,15 @@ public class DiaryController {
 	
 	
 	/**다이어리 상세 조회하기*/
-	//@RequestMapping("/diaryRead/{diaryId}")
-	//public String 
+	@RequestMapping("/diaryRead/{diaryId}")
+	public Map<String, Object> selectByDiaryId(@PathVariable Long diaryId) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		Diary diary =diaryservice.selectById(diaryId);
+		List<DiaryLine> dlist = diary.getDiaryLineList();
+		//List<DiaryLineDTO> diarylineList = dlist.
+		return map;
+	}
 	
 	
 	/**다이어리 삭제하기*/
