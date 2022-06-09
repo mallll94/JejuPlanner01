f123ae384412ee033027a47ec84f97ef5e6b057f
jeonghyeon <wjdgsu53@naver.com>
2022-06-09 09:24:03 +0900

    일정인원 타입 수정

--- src/main/java/kosta/mvc/service/UserServiceImpl.java
+++ src/main/java/kosta/mvc/service/UserServiceImpl.java
@@ -1,5 +1,10 @@
 package kosta.mvc.service;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
@@ -8,6 +13,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import com.querydsl.core.BooleanBuilder;
 
 import kosta.mvc.domain.QUsers;
@@ -148,17 +155,54 @@ public class UserServiceImpl implements UserService {
 	}
 	
 	@Override
-	public String kakaoGetAccessToken(String auth) {
+	public String kakaoGetAccessToken(String code) {
 		String access_Token ="";
 		String refresh_Token="";
 		String reqURL ="https://kauth.kakao.com/oauth/token";
 		
-//		try {
-//			URL url = new URL(reqURL);
-//			//HttpURLConnection conn = 
-//		}
-		
-		return null;
+		try {
+			URL url = new URL(reqURL);
+			HttpURLConnection conn =  (HttpURLConnection)url.openConnection();
+			
+			//HttpURLConnection 설정 값 셋팅
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+
+
+            // buffer 스트림 객체 값 셋팅 후 요청
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+            StringBuilder sb = new StringBuilder();
+            sb.append("grant_type=authorization_code");
+            sb.append("&client_id=REST API 앱키 입력");  //앱 KEY VALUE
+            sb.append("&redirect_uri=http://localhost:8080/kakao_callback"); // 앱 CALLBACK 경로
+            sb.append("&code=" + code);
+            bw.write(sb.toString());
+            bw.flush();
+
+            //  RETURN 값 result 변수에 저장
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String br_line = "";
+            String result = "";
+
+            while ((br_line = br.readLine()) != null) {
+                result += br_line;
+            }
+
+            JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(result);
+
+
+            // 토큰 값 저장 및 리턴
+            access_Token = element.getAsJsonObject().get("access_token").getAsString();
+            refresh_Token = element.getAsJsonObject().get("refresh_token").getAsString();
+
+            br.close();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return access_Token;
 		
 	}
 
