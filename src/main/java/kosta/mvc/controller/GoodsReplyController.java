fa61e4583e999381cab315cd2d08939bb4e0a6db
이아리 <71698417+LEEAHRI@users.noreply.github.com>
2022-06-09 10:58:19 +0900

    상품후기 Controller 작성중.

--- src/main/java/kosta/mvc/controller/GoodsReplyController.java
+++ src/main/java/kosta/mvc/controller/GoodsReplyController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -24,20 +25,34 @@ public class GoodsReplyController {
 	private final GoodsService goodsService;
 	private final UserService userService;
 	
+	/**
+	 * 관리자 페이지.
+	 * @param model
+	 * @return
+	 */
 	@RequestMapping("/admin/goodsReply_Admin")
 	public ModelAndView listForAdmin(Model model) {
 		List<GoodsReply> goodsReplyList = goodsReplyService.getAllGoodsReply();
-		System.out.println("test");
 		List<Goods> goodsList = goodsService.getAllGoods();
-		System.out.println("test");
 		List<Users> usersList = userService.selectAll();
-		System.out.println("test");
 		model.addAttribute("goodsReplyList", goodsReplyList);
-		System.out.println("test");
 		model.addAttribute("goodsList", goodsList);
-		System.out.println("test");
 		
 		return new ModelAndView("admin/goodsReply_Admin");
+		
+	/**
+	 * 관리자 후기 삭제하기
+	 */
+		
+		
+	/**
+	 * 후기 작성하기 (사용자)
+	 */
+		
+		
+	/**
+	 * 후기 삭제하기 (사용자)
+	 */
 	}
 			
 }
