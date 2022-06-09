bf6a0359f5bea42cd60589793b879d1d0daf1498
이아리 <71698417+LEEAHRI@users.noreply.github.com>
2022-06-09 10:56:46 +0900

    TimeDeal Serive 작성 (impl 채우기).

--- src/main/java/kosta/mvc/service/TimeDealService.java
+++ src/main/java/kosta/mvc/service/TimeDealService.java
@@ -1,5 +1,34 @@
 package kosta.mvc.service;
 
-public interface TimeDealService {
+import java.time.LocalDate;
+import java.util.List;
 
+import kosta.mvc.domain.TimeDeal;
+
+public interface TimeDealService {
+	/**
+	 * 핫딜 상품 조회하기
+	 */
+	public List<TimeDeal> getAllTimeDeal();
+
+	/**
+	 * 핫딜 상품 등록하기
+	 */
+	public void addTimeDeal(TimeDeal timeDeal);
+	
+	/**
+	 * 핫딜 수정하기
+	 */
+	public void updateTimeDeal(TimeDeal timeDeal);
+	
+	/**
+	 * 핫딜 삭제하기
+	 */
+	public void deleteTimeDeal(Long timedaelId);
+	
+	/**
+	 * 핫딜 판매중만 조회하기
+	 */
+	public void getTimeDealByCanBuy (LocalDate userEndDate);
+	
 }
